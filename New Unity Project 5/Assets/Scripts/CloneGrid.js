7ef275563afa4193b837df5b06843db4722cf69f
manlok1992 <manlok@Mans-Mac-mini-2.local>
2014-12-15 12:18:57 +0800

    update gitinore and add some function for touch

--- New Unity Project 5/Assets/Scripts/CloneGrid.js
+++ New Unity Project 5/Assets/Scripts/CloneGrid.js
@@ -17,9 +17,11 @@ function Start () {
 	for(var i = 0; i < 4; i++) {
 		for(var j = 0; j < 4; j++) {
 			var g = GameObject.Instantiate(prefab, Vector2(-2.5+(2*i), -2.5+(2*j)), Quaternion.identity);
-			g.AddComponent("id");
-			g.AddComponent("showBall");
+			g.AddComponent("TouchBall");
 			listGrid.Add(g);
+			var touchBall = g.GetComponent(TouchBall);
+			var index = listGrid.FindIndex(function(go:GameObject) go == g);
+			touchBall.touchIndex = index;
 		}
 	}
 	for(var temp:GameObject in listGrid) {
@@ -32,17 +34,29 @@ function Update () {
 }
 
 function shuffle() {
-	if(!isRand) {
+	if(!isRand) { 
 		isRand = true;
-		var tempRand:int;
+		var tempRand:int; 
 		for(var i = 0; i < randomCount; i++) {
 			var rand = Random.Range(0, listGrid.Count);
 			for(var tempRanNum:int in randomedNumArr) {
 				while(rand == tempRanNum) {
-					rand = Random.Range(1, listGrid.Count+1);
+					rand = Random.Range(0, listGrid.Count);
 				}	
 			}
 			randomedNumArr.Add(rand);
+			TouchBall.ballIndex[i] = rand;
+		}
+	}
+	ShowBall();
+}
+
+function ShowBall() {
+	if(randomedNumArr.Count == 4) {
+		for(var i = 0; i < randomedNumArr.Count;) {
+			listGrid[randomedNumArr[i]].transform.FindChild("Pic").active = true;
+			yield new WaitForSeconds(0.8f);
+			i++;
 		}
 	}
 }
---
+++ New Unity Project 5/Assets/Scripts/TouchBall.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+static public var ballIndex:int[] = [0,0,0,0];
+
+public var touchIndex:int;
+
+function Start () {
+	
+}
+
+function Update () {
+
+}
+
+function OnMouseDown() {
+	Debug.Log("Touch Index = "+touchIndex);
+}
